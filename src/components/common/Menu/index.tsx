d456f2bceecb4c14af51bc9bd0e69e9c930179ec
peterkwkwan <peterkwkwan@gmail.com>
2023-04-12 00:15:47 +0800

    feat: reorganize folder structure

---
+++ src/components/common/Menu/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+interface Props {
+  children: React.ReactNode
+}
+
+export const Menu = ({ children }: Props) => {
+  return <div className={}>{children}</div>
+}
