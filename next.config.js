2634cb6e5304e9b0d123741e3655bf65dc3f1cae
peterkwkwan <peterkwkwan@gmail.com>
2023-04-17 11:28:30 +0800

    feat: use src folder

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    prependData: `@import "variables.scss";`,
+    prependData: '@import "@/styles/variables.scss";',
   },
 }
 
